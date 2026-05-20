c255bc28ef40a6a5386416516b08a348152f61c4
Samoraaa <filipedelimasamora@gmail.com>
2026-05-20 20:34:26 -0300

    Sistema Bancario

--- ConsoleApp5/obj/Debug/net10.0/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/obj/Debug/net10.0/ConsoleApp5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b06a85a25b482f8f1da7abc641c697c17b3597")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
